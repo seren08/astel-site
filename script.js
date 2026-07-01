af267dcd4b48763c47f649ca4017c6716805f654
seren08 <serenkagami@gmail.com>
2026-07-02 00:54:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-function createStar(){
+const video = document.getElementById("bgvideo");
 
+if(video){
+    video.play().catch(()=>{
+        document.addEventListener("touchstart",()=>{
+            video.play();
+        },{once:true});
+    });
+}
+
+setInterval(()=>{
     const star=document.createElement("div");
-
     star.className="star";
-
     star.style.left=Math.random()*window.innerWidth+"px";
-
-    star.style.animationDuration=(2+Math.random()*3)+"s";
-
-    star.style.opacity=Math.random();
-
+    star.style.animationDuration=(2+Math.random()*2)+"s";
     document.body.appendChild(star);
 
     setTimeout(()=>{
         star.remove();
-    },5000);
+    },4500);
 
-}
-
-setInterval(createStar,120);
+},250);
