d2a3ca6964b1f6fdb685050e1681ad903b1112aa
seren08 <serenkagami@gmail.com>
2026-07-01 23:08:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function createStar(){
 
+    const star=document.createElement("div");
+
+    star.className="star";
+
+    star.style.left=Math.random()*window.innerWidth+"px";
+
+    star.style.animationDuration=(2+Math.random()*3)+"s";
+
+    star.style.opacity=Math.random();
+
+    document.body.appendChild(star);
+
+    setTimeout(()=>{
+        star.remove();
+    },5000);
+
+}
+
+setInterval(createStar,120);
